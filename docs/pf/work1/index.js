92236f4f2a772f35fe701d080a15f6ef6a985787
mingzyz <verbietenn@gmail.com>
2021-07-19 17:39:27 +0900

    별 애니메이션 추가

--- docs/pf/work1/index.js
+++ docs/pf/work1/index.js
@@ -1,40 +1,65 @@
 $(function () {
 
-function Alarm_AnimationInit() {
+  function Alarm_AnimationInit() {
 
-  gsap.fromTo('.alarm', {
-    opacity: 0,
-    scaleX: 0,
-    scaleY: 0
-  }, {
-    opacity: 1,
-    scaleX: 1,
-    scaleY: 1,
-    scrollTrigger: {
-      trigger: '.alarm',
-      start:'0% 50%',
-      end:'100% 30%'
-    },
-    duration: 0.5
-  });
+    gsap.fromTo('.alarm', {
+      opacity: 0,
+      scale: 0,
+    }, {
+      opacity: 1,
+      scale: 1,
+      scrollTrigger: {
+        trigger: '.alarm',
+        start: '0% 50%',
+        end: '100% 30%'
+      },
+      duration: 0.5
+    });
 
-}
+  }
 
-function Mobile_SlideInit() {
-  const swiper = new Swiper('.slide .swiper-container', {
-    loop: true,
-    autoplay: {
-      delay:2000,
-      disableOnInteraction:false
-    },
-    slidesPerView: 5,
-    spaceBetween: 20
-  });
-}
+  function Mobile_SlideInit() {
+    const swiper = new Swiper('.slide .swiper-container', {
+      loop: true,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false
+      },
+      slidesPerView: 5,
+      spaceBetween: 20
+    });
+  }
 
 
-Alarm_AnimationInit();
-Mobile_SlideInit();
+  function EvenStars_RotateInit(index) {
+    gsap.from(".star-"+ index +" > img", {
+      ease:"none",
+      duration: 5,
+      rotate: 720,
+      repeat:-1
+    });
+  }
 
+  function OddStars_RotateInit(index) {
+    gsap.from(".star-"+ index +" > img", {
+      ease:"none",
+      duration: 5,
+      rotate: -720,
+      repeat:-1
+    });
+  }
+
+
+  Alarm_AnimationInit();
+  Mobile_SlideInit();
+  for (var i = 0; i < 13; i++)
+  {
+    if(i%2==0) {
+      EvenStars_RotateInit(i);
+    }
+    else {
+      OddStars_RotateInit(i);
+    }
+  }
 
 });
